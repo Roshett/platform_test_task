ef8e24952ddce6e28fde206e437067095cc31c05
roshett <roshette@yandex.ru>
2019-09-05 13:28:19 +0300

    Fix require

--- resources/js/app.js
+++ resources/js/app.js
@@ -7,10 +7,10 @@
 require('./bootstrap');
 
 window.Vue = require('vue');
-
-import Trend from "vuetrend"
+const Trend = require('vuetrend');
 
 Vue.use(Trend)
+
 /**
  * The following block of code may be used to automatically register your
  * Vue components. It will recursively scan this directory for the Vue
